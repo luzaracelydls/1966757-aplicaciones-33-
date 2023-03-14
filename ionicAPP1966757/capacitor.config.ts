62cc3c66171fc83b7c5ccb99235b297e944e671d
Adrian Alejandro Cruz Alanis <alejandrocruzalanis@gmail.com>
2023-03-13 18:29:34 -0600

    ionic
    se hace creacion de una aplicacion ionic con framework angular para la clase de aplicaciones moviles

---
+++ ionicAPP1966757/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionicAPP1966757',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
