169c2c65d51867bf788b78ab035472f7a4d35bf6
Adrian Alejandro Cruz Alanis <alejandrocruzalanis@gmail.com>
2023-05-04 17:48:31 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic PIA1966757',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
