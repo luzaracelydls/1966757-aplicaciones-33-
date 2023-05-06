45f46c2e353a28c0189fd57d6904449e4bc8f500
Adrian Alejandro Cruz Alanis <alejandrocruzalanis@gmail.com>
2023-05-06 01:24:20 -0600

    version 2
    se agrega firebase atraves de un npm

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBhniUU7g0-lhpXLp5hYcVZlDNhmE0p5Bk",
+    authDomain: "ionic-pia-1966757.firebaseapp.com",
+    projectId: "ionic-pia-1966757",
+    storageBucket: "ionic-pia-1966757.appspot.com",
+    messagingSenderId: "95688972710",
+    appId: "1:95688972710:web:1224a89e5bd6480e789767"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBhniUU7g0-lhpXLp5hYcVZlDNhmE0p5Bk",
+    authDomain: "ionic-pia-1966757.firebaseapp.com",
+    projectId: "ionic-pia-1966757",
+    storageBucket: "ionic-pia-1966757.appspot.com",
+    messagingSenderId: "95688972710",
+    appId: "1:95688972710:web:1224a89e5bd6480e789767"
+  }
 };
 
 /*
