5d9e8be429aa2e774020d4d09e986bef61450670
Adrian Alejandro Cruz Alanis <alejandrocruzalanis@gmail.com>
2023-05-09 00:24:45 -0600

    version 5
    se crea una interface de usuario y un servicio de auth con el q se trabajara despues y se agrega firebase.json al proyecto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'ionic-pia-1966757',
+    appId: '1:95688972710:web:1224a89e5bd6480e789767',
+    storageBucket: 'ionic-pia-1966757.appspot.com',
+    apiKey: 'AIzaSyBhniUU7g0-lhpXLp5hYcVZlDNhmE0p5Bk',
+    authDomain: 'ionic-pia-1966757.firebaseapp.com',
+    messagingSenderId: '95688972710',
+  },
   production: true,
   firebaseConfig : {
     apiKey: "AIzaSyBhniUU7g0-lhpXLp5hYcVZlDNhmE0p5Bk",
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ionic-pia-1966757',
+    appId: '1:95688972710:web:1224a89e5bd6480e789767',
+    storageBucket: 'ionic-pia-1966757.appspot.com',
+    apiKey: 'AIzaSyBhniUU7g0-lhpXLp5hYcVZlDNhmE0p5Bk',
+    authDomain: 'ionic-pia-1966757.firebaseapp.com',
+    messagingSenderId: '95688972710',
+  },
   production: false,
   firebaseConfig : {
     apiKey: "AIzaSyBhniUU7g0-lhpXLp5hYcVZlDNhmE0p5Bk",
